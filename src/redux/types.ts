03c2c103c6474deb46e325a8581663f028099d4c
fumi uk <dcfumiya.238@gmail.com>
2021-09-16 09:00:27 +0900

    todo

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,3 +1,18 @@
 import { VISIBILITY_FILTERS } from "../constants";
 
 export type VisibilityFilterTypes = typeof VISIBILITY_FILTERS[keyof typeof VISIBILITY_FILTERS];
+
+export type TodoState = {
+  allIds: Array<Number>,
+  byIds: { [key: string]: TodoItemState }
+};
+
+export type TodoItemState = {
+  content: string;
+  completed: boolean;
+};
+
+export type State = {
+  visibilityFilter: VisibilityFilterTypes;
+  todo: TodoState;
+};
