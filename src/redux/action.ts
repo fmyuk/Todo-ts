53fd8f34081599fcd4a50408c39667a88a63f244
fumi uk <dcfumiya.238@gmail.com>
2021-09-21 09:00:20 +0900

    todo

--- src/redux/action.ts
+++ src/redux/action.ts
@@ -1,4 +1,5 @@
 import { ActionTypes } from "./actionTypes";
+import { VisibilityFilterTypes } from "./types";
 
 let nextTodoIds = 0;
 
@@ -17,3 +18,31 @@ export const addTodo = (content: string): AddTodoAction => ({
     content
   }
 });
+
+type ToggleTodoAction = {
+  type: ActionTypes.TOGGLE_TODO,
+  payload: {
+    id: number;
+  }
+};
+
+export const toggleTodo = (id: number): ToggleTodoAction => ({
+  type: ActionTypes.TOGGLE_TODO,
+  payload: {
+    id
+  }
+});
+
+type SetFilterAction = {
+  type: ActionTypes.SET_FILETER,
+  payload: {
+    filter: VisibilityFilterTypes
+  }
+}
+
+export const SetFilterAction = (filter: VisibilityFilterTypes): SetFilterAction => ({
+  type: ActionTypes.SET_FILETER,
+  payload: { filter }
+});
+
+export type TodoActions = AddTodoAction | ToggleTodoAction | SetFilterAction;
