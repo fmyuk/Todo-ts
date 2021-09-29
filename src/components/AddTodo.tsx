c5fee0747d740e0789e2a90b66db34859379dd33
fumi uk <dcfumiya.238@gmail.com>
2021-09-30 08:40:16 +0900

    Todo

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+type AddTodoState = {
+  input: string
+};
+
+class AddTodo extends React.Component<{}, AddTodoState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {input: ""}
+  }
+
+  updateInput = (input: string) => {
+    this.setState({ input });
+  };
+
+  handleAddTodo = () => {
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.updateInput(e.target.value)}
+          value={this.state.input}
+        />
+        <button className="add-todo" onClick={this.handleAddTodo}>
+          Add Todo
+        </button>
+      </div>
+    )
+  }
+}
